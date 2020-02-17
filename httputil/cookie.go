1fc381c61fd24f812f9232357a37ee60b9580e67
Syaiful Bahri <sbahri@indo-soft.com>
2020-02-17 16:41:12 +0700

    add httputil

---
+++ httputil/cookie.go
@@ -0,0 +1,33 @@
+package httputil
+
+import (
+	"net/http"
+)
+
+// CookieOptions store configuration for setting a HTTP cookie
+type CookieOptions struct {
+	Path     string
+	Domain   string
+	Secure   bool
+	HttpOnly bool
+}
+
+// DefaultCookieOptions is a default value for CookieOptions
+var DefaultCookieOptions = &CookieOptions{
+	Path:     "/",
+	Secure:   false,
+	HttpOnly: true,
+}
+
+// NewCookieFromOptions create http.Cookie
+func NewCookieFromOptions(name, value string, maxAge int, options *CookieOptions) *http.Cookie {
+	return &http.Cookie{
+		Name:     name,
+		Value:    value,
+		Path:     options.Path,
+		Domain:   options.Domain,
+		MaxAge:   maxAge,
+		Secure:   options.Secure,
+		HttpOnly: options.HttpOnly,
+	}
+}
